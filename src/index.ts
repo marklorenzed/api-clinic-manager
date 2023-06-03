86c04087161069afe0a87410cbfd8b2e7c2faac1
marklorenzed <marklorenzed@gmail.com>
2023-06-03 12:23:12 +0800

    Modify index.js

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { config } from "dotenv";
 import { createApp } from "./utils/createApp";
+import express, { Express } from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import routes from "./routes";
+import session from "express-session";
 
 config();
 
@@ -9,21 +16,39 @@ if (!process.env.PORT) {
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
 
-async function main() {
-  try {
-    const app = createApp();
+const app = express();
 
-    app.listen(PORT, () => {
-      console.log(`Running on Port ${PORT}`);
-    });
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
-    return app;
-  } catch (error) {
-    console.log(error);
-  }
-}
+// Enable Parsing Middleware for Requests
+app.use(express.json());
+app.use(express.urlencoded());
 
-const app = main();
+// Enable CORS
+app.use(cors(corsOptions));
+
+app.use(
+  session({
+    secret: "qwepqowiepqoweipqowie",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60000 * 60 * 24 * 7,
+    },
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.use("/api", routes);
+app.listen(PORT, () => {
+  console.log("[INFO] Server Running on port:", PORT);
+});
 
 module.exports = app;
-
